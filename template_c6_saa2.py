549a77f44e2722190127540a8f2e4bb46c939b97
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-21 11:42:21 +0530

    Create template_c6_saa2.py

---
+++ template_c6_saa2.py
@@ -0,0 +1,12 @@
+# Create a list 'numbers'
+
+# Create a variable 'sum'
+# Initializing it to '0'
+
+
+# Create a 'for' loop to access every element in the list
+
+  # Add 'num' to 'sum'
+  
+
+# Print the value of 'sum'
